2eb37c63fc73350cdfb2c162b4a6e611cdc10cbc
Serlest <timofeev.ph@outlook.com>
2026-04-02 20:40:26 +0200

    Initial Commit

---
+++ MogilSerlest/MogilSerlestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogilSerlestApp.swift
+//  MogilSerlest
+//
+//  Created by Philipp Timofeev on 02.04.26.
+//
+
+import SwiftUI
+
+@main
+struct MogilSerlestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
